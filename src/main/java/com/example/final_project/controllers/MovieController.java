35c0021450cf6b9d57214be106d8e52245a1a606
root <averkievnv@gmail.com>
2022-09-15 23:26:46 +0300

    Setting classes

--- src/main/java/com/example/final_project/controllers/MovieController.java
+++ src/main/java/com/example/final_project/controllers/MovieController.java
@@ -4,7 +4,6 @@ import com.example.final_project.models.Movie;
 import com.example.final_project.repositories.DirectorRepository;
 import com.example.final_project.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
